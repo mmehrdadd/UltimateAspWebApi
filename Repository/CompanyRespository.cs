e0e758f08c6b59e87c613c5a238eee09a543eb42
m.sadeghpour <m.sadeghpour17@gmail.com>
2025-01-05 13:10:00 +0330

    Repository Pattern Implementation

---
+++ Repository/CompanyRespository.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class CompanyRespository : RepositoryBase<Company> , ICompanyRepository
+    {
+        public CompanyRespository(RepositoryContext repositoryContext)
+            :base(repositoryContext) 
+        {
+
+        }
+    }
+}
